b75e906773c12b038d4c9b56d1404c58d9482bea
Rahul Krishna <75737935+RahulKrishna8029@users.noreply.github.com>
2023-11-26 21:50:56 +0530

    utility update with the pull request

--- csv_utils.py
+++ csv_utils.py
@@ -1,6 +1,57 @@
 import csv
 import json
 
+def write_and_process_changes(changes, change_type, pull_request_details):
+    if len(changes) == 0:
+        return
+
+    # Assuming construct_kg, Stanford_Relation_Extractor, and create_csv functions exist
+    construct_kg(changes)
+    Stanford_Relation_Extractor()
+    create_csv(pull_request_details['date_closed'], pull_request_details['number'], pull_request_details['author'], change_type)
+
+    # Additional step: create cleaned CSV
+    create_cleaned_csv('commits.csv', 'cleaned_commits.csv')
+
+def create_cleaned_csv(input_path, output_path):
+    with open(input_path, 'r') as inp, open(output_path, 'w') as out:
+        field_names = ['Entity 1', 'Relationship', 'Entity2', 'Date', 'Sha', 'Change']
+        csv_reader = csv.DictReader(inp)
+        writer = csv.DictWriter(out, fieldnames=field_names)
+        writer.writeheader()
+        for rows in csv_reader:
+            if rows['Change'] in ('addition', 'deletion'):
+                csv_row = {key: value for key, value in rows.items() if key not in ['Type']}
+                writer.writerow(csv_row)
+
+def convert_csv_to_json(input_path, output_path):
+    data = {
+        "commits": {}
+    }
+
+    with open(input_path) as f:
+        csv_reader = csv.DictReader(f)
+
+        for rows in csv_reader:
+            if rows['Change'] not in ('addition', 'deletion'):
+                continue
+
+            key = rows['Sha']
+
+            if key not in data['commits']:
+                commit_data = {
+                    'date': rows['Date'],
+                    'additions': [],
+                    'deletions': []
+                }
+                data['commits'][key] = commit_data
+
+            data['commits'][key][rows['Change'] + 's'].append(rows)
+
+    with open(output_path, 'w', encoding='utf-8') as jsonf:
+        jsonf.write(json.dumps(data, indent=4))
+"""
+
 def convert_to_json(path, out_path):
     data = {
         "commits":{}
@@ -54,7 +105,7 @@ def clean_readme_csv(input_path, output_path):
             del triplet["Type"], triplet["Change"], triplet["Date"], triplet["Sha"]            
             writer.writerow(triplet)
 
-
+"""
 if __name__ == "__main__":
     convert_to_json('input.csv')
 
